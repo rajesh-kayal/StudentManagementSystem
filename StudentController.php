32a1b5f64b8a509dddc4856c789788d001318a82
Rajesh Kayal <119810174+rajesh-kayal@users.noreply.github.com>
2023-07-22 11:35:00 +0530

    Update StudentController.php

--- StudentController.php
+++ StudentController.php
@@ -3,13 +3,31 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use Illuminate\Validation\Rules\Password;
 class StudentController extends Controller
 {
     public function index(){
         return view('index');
     }
     public function insert(Request $req){
+        $validate=$req->validate([
+            'name'=>'required',
+            'email'=>'required|email',
+            'phone'=>'required|numeric',
+            'gen'=>'required|in:Male,Female',  //radio button
+            'lang'=>'required|array',   //selectbox
+            'ch'=>'required|array',  //checkbox all
+            'ch.*'=> 'in:10th,12th,Gradutation,Post Gradutation', //checkbox alll
+            'avatar' => 'required|mimes:jpg,jpge,png,gif|max:3000',
+            'pass'=>['required','confirmed',Password::min(8)
+                                                            ->letters() // Require at least one letter...
+                                                            ->mixedCase() // Require at least one uppercase and one lowercase letter...
+                                                        ->numbers() // Require at least one number...
+                                                    ->symbols()// Require at least one symbol...
+                                                ],
+            'pass_confirmation'=>'required'
+        ]);
+        
         $file=$req->file('avatar');
         $fileDir='./images/';
         $fileName=time()."_".$file->getCLientOriginalName();
